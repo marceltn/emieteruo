18fc193c5cfe3d54f066c6b20e683b326a7058e2
Marcel Nakamine <marcel.nakamine@gmail.com>
2017-09-28 19:06:54 -0300

    adding OurHistory secsion

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -2,17 +2,20 @@
   <div id="landing-page">
     <welcome-header></welcome-header>
     <intro-wedding></intro-wedding>
+    <our-history></our-history>
   </div>
 </template>
 
 <script>
   import WelcomeHeader from './WelcomeHeader.vue'
   import IntroWedding from './IntroWedding.vue'
+  import OurHistory from './OurHistory.vue'
 
   export default {
     components: {
       WelcomeHeader,
-      IntroWedding
+      IntroWedding,
+      OurHistory
     }
   }
 </script>
