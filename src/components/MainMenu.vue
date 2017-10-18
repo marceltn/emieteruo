b62b7099779623ae6a0254de384fa042f1993435
Marcel Nakamine <marcel.nakamine@gmail.com>
2017-10-18 18:15:59 -0200

    Adding vue-scrollto dependency

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,16 +1,16 @@
 <template>
 <b-navbar top id="mainNav" toggleable="sm" sticky="true" type="dark" variant="primary">
   <b-nav-toggle target="nav_collapse"></b-nav-toggle>
-  <b-navbar-brand href="#page-top" class="navbar-brand page-scroll">EMIETERUO</b-navbar-brand>
+  <b-navbar-brand v-scroll-to="'header'" href="#header" class="navbar-brand">EMIETERUO</b-navbar-brand>
 
   <b-collapse is-nav id="nav_collapse">
     <!-- Right aligned nav items -->
     <b-nav is-nav-bar class="ml-auto">
-      <b-nav-item right href="#our-history">Sobre nós</b-nav-item>
-      <b-nav-item right href="#groomsMenBridesMaids">Padrinhos e Madrinhas</b-nav-item>
-      <b-nav-item right href="#thanks">Agradecimentos</b-nav-item>
-      <b-nav-item right href="#photos">Fotos do casamento</b-nav-item>
-      <b-nav-item right href="#wedding">Detalhes do casamento</b-nav-item>
+      <b-nav-item right v-scroll-to="'#our-history'" href="#our-history">Sobre nós</b-nav-item>
+      <b-nav-item right v-scroll-to="'#groomsMenBridesMaids'" href="#groomsMenBridesMaids">Padrinhos e Madrinhas</b-nav-item>
+      <b-nav-item right v-scroll-to="'#thanks'" href="#thanks">Agradecimentos</b-nav-item>
+      <b-nav-item right v-scroll-to="'#photos'" href="#photos">Fotos do casamento</b-nav-item>
+      <b-nav-item right v-scroll-to="'#wedding'" href="#wedding">Detalhes do casamento</b-nav-item>
     </b-nav>
 
   </b-collapse>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
+import VueScrollTo from 'vue-scrollto'
 
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -11,10 +12,24 @@ import 'bootstrap/dist/css/bootstrap.css'
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 
+Vue.use(VueScrollTo, {
+  container: 'body',
+  duration: 350,
+  easing: 'ease-out',
+  offset: 0,
+  cancelable: true,
+  onDone: false,
+  onCancel: false,
+  x: false,
+  y: true
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: {
+    App
+  }
 })
